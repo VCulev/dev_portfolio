02f0a8e1f27b38b8b54e79a02b70f97d78b38af8
Slavic Culev <slavicculev@Slavics-MacBook-Air.local>
2024-07-24 18:51:37 +0300

    Added styling, buttons, grid and updated Hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,20 @@
 import Hero from "@/components/Hero";
+import FloatingNavbar from "@/components/ui/FloatingNavbar";
+import {FaHome, FaInfo} from "react-icons/fa";
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center
-    items-center flex-col overflow-hidden bg-white mx-auto sm:px-10 px-5">
+    items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
+          <FloatingNavbar
+              navItems={
+              [
+                  {name: 'Home', link: '#', icon: <FaHome/>},
+                  {name: 'About', link: '#', icon: <FaInfo/>}
+              ]
+              }
+          />
         <Hero />
       </div>
     </main>
